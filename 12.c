adf53b31702d544049244669e2a5a86c64e38271
Saranyapushparaj <psaranya2806@gmail.com>
2016-07-10 12:54:14 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+char name={"TREE"};
+int thickness;
+int branches;
+char color[];
+int angle;
+printf("enter the thickness of the tree");
+scanf("%d,&thickness of the tree");
+printf("enter the branches of the tree");
+scanf("%d,branches of the tree");
+printf("enter the color of the tree");
+scanf("%c,&color of the tree");
+printf("enter the angle of the tree");
+scanf("%d,&angle of the tree");
+if(thickness>23)
+{
+if (branches>20)
+{
+if(angle<=45)
+{
+printf("\n Angle of the tree is:");
+}
+printf("\n Branches of the tree is:");
+}
+printf("\n Thickness of the tree is:");
+}
+getch()
+}
